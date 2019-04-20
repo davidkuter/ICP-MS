739501ed5123f6870258cf9a4e512b7a8b5f9f44
davidkuter <david.kuter@gmail.com>
2019-04-19 20:16:09 -0400

    Added setup.py, requirements.txt and dist package

--- format_icpms_map_data/setup.py
+++ format_icpms_map_data/setup.py
@@ -3,13 +3,15 @@ import setuptools
 setuptools.setup(
     name="format_icpms_map_data",
     version="1.0.0",
-    scripts="format_icpms_map_data",
     author="David Kuter",
     author_email="david.kuter@gmail.com",
     description="Tool to format LA-ICP-MS data",
     long_description="Formats LA-ICP-MS csv files into separate elemental matrix files for 2D contour plotting",
     url="https://github.com/davidkuter/ICP-MS/tree/master/",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    entry_pionts={'console_scripts': ['format_icpms_map_data = format_icpms_map_data:main']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Free for non-commercial use  ",
